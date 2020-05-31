2e1263cb8b263dbf799ba224dc3f0b4d05949766
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-05-31 22:07:12 +0530

    String Streams

--- src/main/java/io/yadnyesh/yt/streams/JavaStreams.java
+++ src/main/java/io/yadnyesh/yt/streams/JavaStreams.java
@@ -1,13 +1,13 @@
 package io.yadnyesh.yt.streams;
 
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class JavaStreams {
     public static void main(String[] args) {
-        IntStream
-                .range(1,10)
-                .skip(5)
-                .forEach(System.out::print);
-        System.out.println();
+        Stream.of("Bharat", "Sweta", "Yadnyesh", "Bindiya", "Varada")
+                .sorted()
+                .findFirst()
+                .ifPresent(System.out::print);
     }
 }
