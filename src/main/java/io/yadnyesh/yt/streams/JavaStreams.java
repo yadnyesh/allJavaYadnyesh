9cdf16ed8b82b99550adde81686ffc117b51cd9c
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-05-31 21:45:15 +0530

    Starting with Java Streams

---
+++ src/main/java/io/yadnyesh/yt/streams/JavaStreams.java
@@ -0,0 +1,12 @@
+package io.yadnyesh.yt.streams;
+
+import java.util.stream.IntStream;
+
+public class JavaStreams {
+    public static void main(String[] args) {
+        IntStream
+                .range(1,10)
+                .forEach(System.out::print);
+        System.out.println();
+    }
+}
