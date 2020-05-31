a1682bab082c3505078e8ae4384215cd3fcc7c33
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-05-31 22:04:31 +0530

    Adding step to skip :)

--- src/main/java/io/yadnyesh/yt/streams/JavaStreams.java
+++ src/main/java/io/yadnyesh/yt/streams/JavaStreams.java
@@ -6,6 +6,7 @@ public class JavaStreams {
     public static void main(String[] args) {
         IntStream
                 .range(1,10)
+                .skip(5)
                 .forEach(System.out::print);
         System.out.println();
     }
