fca08b873e0c02ac0f0bc88b74bb3076c31ec804
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-05-31 22:13:07 +0530

    Misc

--- src/main/java/io/yadnyesh/yt/streams/JavaStreams.java
+++ src/main/java/io/yadnyesh/yt/streams/JavaStreams.java
@@ -1,11 +1,13 @@
 package io.yadnyesh.yt.streams;
 
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import java.util.Arrays;
 
 public class JavaStreams {
     public static void main(String[] args) {
-        Stream.of("Bharat", "Sweta", "Yadnyesh", "Bindiya", "Varada")
+        String[] names = {"Al", "Ankit", "Noopur", "Payal", "Supriya", "Soma", "Sarika", "Hans", "Shivika", "Yogesh",
+                          "Abhinandan", "Suresh", "Aditya", "Rakesh", "Shital"};
+        Arrays.stream(new int[] {1,2,3,4,5,6,7})
+                .map(x -> x*2)
                 .sorted()
                 .forEach(System.out::println);
     }
