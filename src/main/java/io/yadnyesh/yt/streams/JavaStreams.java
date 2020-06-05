44d9ef64e61088e24e1c4dae87f887f9dd504c2b
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-06-05 22:58:18 +0530

    Stream of Integers

--- src/main/java/io/yadnyesh/yt/streams/JavaStreams.java
+++ src/main/java/io/yadnyesh/yt/streams/JavaStreams.java
@@ -1,14 +1,20 @@
 package io.yadnyesh.yt.streams;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class JavaStreams {
     public static void main(String[] args) {
-        String[] names = {"Al", "Ankit", "Noopur", "Payal", "Supriya", "Soma", "Sarika", "Hans", "Shivika", "Yogesh",
-                          "Abhinandan", "Suresh", "Aditya", "Rakesh", "Shital"};
-        Arrays.stream(new int[] {1,2,3,4,5,6,7})
-                .map(x -> x*2)
-                .sorted()
+        List<String> people = Arrays.asList("Al", "Ankit", "Noopur", "Payal", "Supriya", "Soma", "Sarika", "Hans", "Shivika", "Yogesh",
+                          "Abhinandan", "Suresh", "Aditya", "Rakesh", "Shital");
+        people.stream()
+                .map(String::toLowerCase)
+                .filter(x -> x.startsWith("s"))
                 .forEach(System.out::println);
+
+        Arrays.stream(new int[] {2, 4, 6, 8, 10})
+                .map( x -> x * x)
+                .average()
+                .ifPresent(System.out::println);
     }
 }
