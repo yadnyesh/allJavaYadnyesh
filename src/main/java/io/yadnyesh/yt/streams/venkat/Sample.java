689da4ba45585c1c90b08fb0fafe620cabac7624
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-06-19 11:24:59 +0530

    Parallel Stream

--- src/main/java/io/yadnyesh/yt/streams/venkat/Sample.java
+++ src/main/java/io/yadnyesh/yt/streams/venkat/Sample.java
@@ -13,14 +13,14 @@ public class Sample {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return number * 1;
+        return number;
     }
 
     public static void main(String[] args) {
-
         List<Integer> integerList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
         integerList.stream()
-                .map(e -> transform(e))
-                .forEach(System.out::println);
+                .parallel()
+                .map(Sample::transform)
+                .forEach(e -> System.out.println(e + " " + Thread.currentThread().getName()));
     }
 }
