cefad93ce7706b9b6b71b261d06d856dc75a4856
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-06-19 11:13:25 +0530

    Print a number every second

--- src/main/java/io/yadnyesh/yt/streams/venkat/Sample.java
+++ src/main/java/io/yadnyesh/yt/streams/venkat/Sample.java
@@ -3,20 +3,24 @@ package io.yadnyesh.yt.streams.venkat;
 import java.util.Arrays;
 import java.util.List;
 
+import static java.lang.Thread.sleep;
+
 public class Sample {
 
     public static int transform(int number) {
+        try {
+            sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return number * 1;
     }
 
     public static void main(String[] args) {
 
-        List<Integer> integerList = Arrays.asList(1,2,3,4,5,6,7,8,9,0);
-
-        System.out.println(integerList.stream()
-            .filter(e -> e%2 ==0)
-            .mapToInt(e -> e * 2)
-            .sum()
-        );
+        List<Integer> integerList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        integerList.stream()
+                .map(e -> transform(e))
+                .forEach(System.out::println);
     }
 }
