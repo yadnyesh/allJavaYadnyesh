b0c5a078cd55228ac4b01fe5502f465cddac29e7
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-07-10 11:14:07 +0530

    Implemented Functional Interface

--- src/main/java/io/yadnyesh/udemy/funcreactmodern/LambdaExample.java
+++ src/main/java/io/yadnyesh/udemy/funcreactmodern/LambdaExample.java
@@ -2,7 +2,12 @@ package io.yadnyesh.udemy.funcreactmodern;
 
 public class LambdaExample {
     public static void main(String[] args) {
-        MyFunInterface myFunInterface = () -> System.out.println("hello");
-        myFunInterface.myMethod();
+//        MyFunInterface myFunInterface = () -> System.out.println("hello");
+//        myFunInterface.myMethod();
+        onTheFly(() -> System.out.println("Test"));
+    }
+
+    public static void onTheFly(MyFunInterface fun) {
+        fun.myMethod();
     }
 }
