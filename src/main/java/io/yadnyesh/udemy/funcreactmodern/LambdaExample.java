4911790f1ea1e81c964a30cd7d4f916a84bc18c3
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-07-11 17:12:05 +0530

    Lambda SAM

--- src/main/java/io/yadnyesh/udemy/funcreactmodern/LambdaExample.java
+++ src/main/java/io/yadnyesh/udemy/funcreactmodern/LambdaExample.java
@@ -2,12 +2,7 @@ package io.yadnyesh.udemy.funcreactmodern;
 
 public class LambdaExample {
     public static void main(String[] args) {
-//        MyFunInterface myFunInterface = () -> System.out.println("hello");
-//        myFunInterface.myMethod();
-        onTheFly(() -> System.out.println("Test"));
-    }
-
-    public static void onTheFly(MyFunInterface fun) {
-        fun.myMethod();
+        Name name = () -> System.out.println("Yadnyesh, hello from Lambda SAM");
+        name.myName();
     }
 }
