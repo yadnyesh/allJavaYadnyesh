3f0e3d98f012b97958fd04443f0611bd65078790
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-07-12 17:26:18 +0530

    Constructor Reference Example

---
+++ src/main/java/io/yadnyesh/udemy/funcreactmodern/ConstructorReference.java
@@ -0,0 +1,20 @@
+package io.yadnyesh.udemy.funcreactmodern;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.function.Function;
+
+@Slf4j
+public class ConstructorReference {
+    public static void main(String[] args) {
+        log.info("Starting execution of the program: "  + Thread.currentThread().getName());
+        Function<Runnable, Thread> threadGenerator = Thread::new;
+        Runnable taskOne = () -> log.info("Task 1 completed"  + Thread.currentThread().getName());
+        Runnable taskTwo = () -> log.info("Task 2 completed "  + Thread.currentThread().getName());
+        Thread threadOne = threadGenerator.apply(taskOne);
+        Thread threadTwo = threadGenerator.apply(taskTwo);
+        threadOne.start();
+        threadTwo.start();
+        threadGenerator.apply(() -> log.info("Compact Form" + Thread.currentThread().getName())).start();
+    }
+}
