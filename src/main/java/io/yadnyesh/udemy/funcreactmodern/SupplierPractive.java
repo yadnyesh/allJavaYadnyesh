02db2a915bbfacaa2bd014456165752f14dce897
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-07-12 12:45:04 +0530

    Supplier Functional Interface

---
+++ src/main/java/io/yadnyesh/udemy/funcreactmodern/SupplierPractive.java
@@ -0,0 +1,15 @@
+package io.yadnyesh.udemy.funcreactmodern;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.function.Supplier;
+
+@Slf4j
+public class SupplierPractive {
+    public static void main(String[] args) {
+        Supplier<String> stringSupplier = () -> "new String by Supplier";
+        Supplier<Double> randonNumber = Math::random;
+        log.info(stringSupplier.get());
+        log.info(randonNumber.get().toString());
+    }
+}
