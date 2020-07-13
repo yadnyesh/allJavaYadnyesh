79b9e928ec113ff4c82de1b3c2cc9bdd2c758f45
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-07-13 12:58:53 +0530

    Operations with Optional

--- src/main/java/io/yadnyesh/udemy/funcreactmodern/Operations.java
+++ src/main/java/io/yadnyesh/udemy/funcreactmodern/Operations.java
@@ -15,5 +15,14 @@ public class Operations {
         log.info(filter.orElse("Not Found"));
         Optional<String> flatMap = optional.flatMap(val -> Optional.of("Replaced FlatMap"));
         log.info(flatMap.orElse("yad not found"));
+        Optional<String> stringOptional = Optional.of("Value");
+        optional.ifPresent(val -> log.info("String of optional IfPresent -> " + val));
+        Optional<String> emptyOptional = Optional.empty();
+        emptyOptional.ifPresentOrElse(log::info, () -> log.info("Value is absent"));
+        optional.stream().forEach(log::info);
+        emptyOptional.or(() -> Optional.of("Empty when using .or()")).ifPresent(log::info);
+        System.out.println(optional.equals(Optional.of("Value")));
+        System.out.println(optional.hashCode());
+        System.out.println(Optional.of("Value").hashCode());
     }
 }
