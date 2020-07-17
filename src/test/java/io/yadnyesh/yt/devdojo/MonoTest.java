50c7215a67e03ec13b1d60aaea0e8f91a7a20c5b
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-07-17 08:23:38 +0530

    Project Setup Complete - Devdojo Project Rector

---
+++ src/test/java/io/yadnyesh/yt/devdojo/MonoTest.java
@@ -0,0 +1,12 @@
+package io.yadnyesh.yt.devdojo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+public class MonoTest {
+    @Test
+    public void test(){
+        log.info("Project Setup Complete for Devdojo - Project Reactor");
+    }
+}
