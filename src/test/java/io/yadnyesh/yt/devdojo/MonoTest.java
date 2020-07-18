34909477931b691c0fd87f7a84c1fe801dd80f3f
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-07-18 11:11:36 +0530

    Check for logs - Mono witj list implementation

--- src/test/java/io/yadnyesh/yt/devdojo/MonoTest.java
+++ src/test/java/io/yadnyesh/yt/devdojo/MonoTest.java
@@ -2,11 +2,19 @@ package io.yadnyesh.yt.devdojo;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Slf4j
 public class MonoTest {
     @Test
     public void test(){
+        String name = "Yadnyesh";
+        List<String> stringList = List.of("Yadnyesh", "Bharat", "Juvekar");
+        Mono<List<String>> mono = Mono.just(stringList).log();
+        mono.subscribe();
+        log.info("Mono {}", mono);
         log.info("Project Setup Complete for Devdojo - Project Reactor");
     }
 }
