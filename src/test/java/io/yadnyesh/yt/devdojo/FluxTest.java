d8ce1622b043c06f72b8bf9ab457d1ad447f2219
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-07-18 16:01:05 +0530

    Subscribing to Flux List

--- src/test/java/io/yadnyesh/yt/devdojo/FluxTest.java
+++ src/test/java/io/yadnyesh/yt/devdojo/FluxTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import java.util.List;
+
 @Slf4j
 public class FluxTest {
 
@@ -21,6 +23,16 @@ public class FluxTest {
     public void FluxSubscriberNumbers() {
         Flux<Integer> flux = Flux.range(1,5)
                 .log();
+        flux.subscribe(i -> log.info("Number: {}", i));
+        log.info("----------------------------------------");
+        StepVerifier.create(flux)
+                .expectNext(1,2,3,4,5)
+                .verifyComplete();
+    }
+
+    @Test
+    public void FluxSubscriberFromList() {
+    Flux<Integer> flux = Flux.fromIterable(List.of(1,2,3,4,5)).log();
         flux.subscribe(i -> log.info("Number: {}", i));
         log.info("----------------------------------------");
         StepVerifier.create(flux)
