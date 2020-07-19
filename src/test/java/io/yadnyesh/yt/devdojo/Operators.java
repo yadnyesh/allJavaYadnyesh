723b678301fc2154b1de76b42d77a2202866a660
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-07-19 11:55:26 +0530

    publishAndSubscribeOnSimple

--- src/test/java/io/yadnyesh/yt/devdojo/Operators.java
+++ src/test/java/io/yadnyesh/yt/devdojo/Operators.java
@@ -83,4 +83,23 @@ public class Operators {
                 .expectNext(1,2,3,4)
                 .verifyComplete();
     }
+
+    @Test
+    public void publishAndSubscribeOnSimple() {
+        Flux<Integer> flux = Flux.range(1,4)
+                .publishOn(Schedulers.single())
+                .map(i -> {
+                    log.info("Map 1 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .subscribeOn(Schedulers.boundedElastic())
+                .map(i -> {
+                    log.info("Map 2 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                });
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectNext(1,2,3,4)
+                .verifyComplete();
+    }
 }
