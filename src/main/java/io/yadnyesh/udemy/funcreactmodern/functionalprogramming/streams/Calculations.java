83921c047c9710e4233f735cee93bdf45d4e1035
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-07-23 09:43:39 +0530

    See Optional empty

--- src/main/java/io/yadnyesh/udemy/funcreactmodern/functionalprogramming/streams/Calculations.java
+++ src/main/java/io/yadnyesh/udemy/funcreactmodern/functionalprogramming/streams/Calculations.java
@@ -2,12 +2,16 @@ package io.yadnyesh.udemy.funcreactmodern.functionalprogramming.streams;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.OptionalInt;
 import java.util.stream.IntStream;
 
 @Slf4j
 public class Calculations {
   public static void main(String[] args) {
-      IntStream sum = IntStream.of(1,2,3,4,5,6,7,8);
-      log.info(String.valueOf(sum.sum()));
+      int sum = IntStream.of(1,2,3,4,5,6,7,8).sum();
+      log.info(String.valueOf(sum));
+
+      OptionalInt maxOptional = IntStream.of().max();
+      log.info(maxOptional.toString());
   }
 }
