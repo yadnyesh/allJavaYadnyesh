8a93ca1439db422a10383fa652f2f464329d4fbb
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-07-23 10:23:37 +0530

    Summary Statistics

--- src/main/java/io/yadnyesh/udemy/funcreactmodern/functionalprogramming/streams/Calculations.java
+++ src/main/java/io/yadnyesh/udemy/funcreactmodern/functionalprogramming/streams/Calculations.java
@@ -2,6 +2,7 @@ package io.yadnyesh.udemy.funcreactmodern.functionalprogramming.streams;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.IntSummaryStatistics;
 import java.util.OptionalInt;
 import java.util.stream.IntStream;
 
@@ -13,5 +14,9 @@ public class Calculations {
 
       OptionalInt maxOptional = IntStream.of().max();
       log.info(maxOptional.toString());
+
+      IntSummaryStatistics intSummaryStatistics = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+              .summaryStatistics();
+      log.info(intSummaryStatistics.toString());
   }
 }
