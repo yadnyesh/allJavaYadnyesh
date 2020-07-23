9b5a5fa1e4b2e10f97e31f7bb089d480fbd1a806
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-07-23 10:41:36 +0530

    InfiniteStreams

--- src/main/java/io/yadnyesh/udemy/funcreactmodern/functionalprogramming/streams/InfiniteStreams.java
+++ src/main/java/io/yadnyesh/udemy/funcreactmodern/functionalprogramming/streams/InfiniteStreams.java
@@ -1,11 +1,16 @@
 package io.yadnyesh.udemy.funcreactmodern.functionalprogramming.streams;
 
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class InfiniteStreams {
   public static void main(String[] args) {
       Stream.iterate(0, i -> i - 1)
               .limit(15)
+              .forEach(System.out::println);
+
+      IntStream.iterate(0, i -> i -1)
+              .limit(500)
               .forEach(System.out::println);
   }
 }
