00810749ee4e6100c3833336f338e84a84891370
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-07-26 21:25:11 +0530

    Collectors - Groupings - Count

--- src/main/java/io/yadnyesh/udemy/funcreactmodern/functionalprogramming/collectors/CollectorsInAction.java
+++ src/main/java/io/yadnyesh/udemy/funcreactmodern/functionalprogramming/collectors/CollectorsInAction.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
@@ -26,6 +27,8 @@ public class CollectorsInAction {
                   Collectors.groupingBy(Employee::getDesignation, Collectors.summingDouble(e -> e.getSalary()))
           );
           log.info(groupByDesignation.toString());
+
+          Function.identity()
 
       } catch (IOException e) {
           log.error(e.getMessage());
