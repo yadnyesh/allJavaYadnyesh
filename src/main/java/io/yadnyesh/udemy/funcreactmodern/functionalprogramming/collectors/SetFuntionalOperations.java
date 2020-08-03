a3d799dba1ffd68f465e93672aa3a368eb5fa65d
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-08-03 11:04:24 +0530

    Set Functional way - Map & Reduce

---
+++ src/main/java/io/yadnyesh/udemy/funcreactmodern/functionalprogramming/collectors/SetFuntionalOperations.java
@@ -0,0 +1,19 @@
+package io.yadnyesh.udemy.funcreactmodern.functionalprogramming.collectors;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Set;
+
+@Slf4j
+public class SetFuntionalOperations {
+  public static void main(String[] args) {
+      Set<Integer> set = Set.of(3,56,7,82,39);
+      set.forEach(s -> log.info(s.toString()));
+      log.info("---------------------------");
+      set.stream().sorted().forEach(s -> log.info(s.toString()));
+      log.info("---------------------------");
+      set.stream()
+              .filter(e -> e%2 ==0)
+              .forEach(e -> log.info(e.toString()));
+  }
+}
