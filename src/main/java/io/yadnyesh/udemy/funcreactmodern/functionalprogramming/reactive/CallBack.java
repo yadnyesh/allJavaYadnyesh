fd8e99bd97815845bfbc86084e4e223e95e35b19
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-09-13 16:40:01 +0530

    Call back Push

--- src/main/java/io/yadnyesh/udemy/funcreactmodern/functionalprogramming/reactive/CallBack.java
+++ src/main/java/io/yadnyesh/udemy/funcreactmodern/functionalprogramming/reactive/CallBack.java
@@ -2,4 +2,8 @@ package io.yadnyesh.udemy.funcreactmodern.functionalprogramming.reactive;
 
 public interface CallBack {
     void call();
+
+    void pushComplete();
+    void pushData(String data);
+    void pushError(Exception exception);
 }
