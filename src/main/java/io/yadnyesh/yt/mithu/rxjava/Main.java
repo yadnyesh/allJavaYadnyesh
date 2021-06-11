b8e15f82762dfd63b0d9fa15830f0e29d454aa09
yadnyesh <yadnyesh.juvekar@gmail.com>
2021-06-11 10:50:46 +0530

    Taking a break for Rx Java

--- src/main/java/io/yadnyesh/yt/mithu/rxjava/Main.java
+++ src/main/java/io/yadnyesh/yt/mithu/rxjava/Main.java
@@ -7,13 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Main {
   public static void main(String[] args) {
-      Observable.just(1,2,3,4,5,6,7,8)
-              //.subscribe(System.out::println);
-                .subscribe(new Consumer<Integer>() {
-                    @Override
-                    public void accept(Integer integer) throws Throwable {
-                        log.info(integer.toString());
-                    }
-                });
+        Thread myThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                log.info("Run...");
+            }
+        });
   }
 }
