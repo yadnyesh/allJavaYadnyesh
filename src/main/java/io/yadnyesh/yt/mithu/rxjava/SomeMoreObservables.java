849b2b8cd814f97e247bc30986493f8a11181e43
yadnyesh <yadnyesh.juvekar@gmail.com>
2021-06-19 07:43:50 +0530

    Connectable Observable

--- src/main/java/io/yadnyesh/yt/mithu/rxjava/SomeMoreObservables.java
+++ src/main/java/io/yadnyesh/yt/mithu/rxjava/SomeMoreObservables.java
@@ -1,6 +1,7 @@
 package io.yadnyesh.yt.mithu.rxjava;
 
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.observables.ConnectableObservable;
 import lombok.extern.slf4j.Slf4j;
 
 
@@ -8,10 +9,17 @@ import lombok.extern.slf4j.Slf4j;
 public class SomeMoreObservables {
   public static void main(String[] args) {
       createColdObservable();
-    //
+      createHotAndColdObservable();
 
 
   }
+
+    private static void createHotAndColdObservable() {
+        ConnectableObservable observable = Observable.just(1,2,3,4,5).publish();
+        observable.subscribe(item -> log.info("createHotAndColdObservable - Observer 1: " + item.toString()));
+        observable.subscribe(item -> log.info("createHotAndColdObservable - Observer 2: " + item.toString()));
+        observable.connect();
+    }
 
     private static void createColdObservable() {
         Observable observable = Observable.just(1,2,3,4,5);
