80d43f0f4eb9d04d70d036484ae203ba9b8801bc
yadnyesh <yadnyesh.juvekar@gmail.com>
2021-07-13 10:59:14 +0530

    Starting with Supplier

--- src/main/java/io/yadnyesh/yt/amigoscode/javafunctionalprogramming/Supplieryad.java
+++ src/main/java/io/yadnyesh/yt/amigoscode/javafunctionalprogramming/Supplieryad.java
@@ -2,6 +2,7 @@ package io.yadnyesh.yt.amigoscode.javafunctionalprogramming;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 @Slf4j
@@ -9,13 +10,14 @@ public class Supplieryad {
 
     public static void main(String[] args) {
        log.info(getDBConnectionUrl());
-       log.info(getDbConnectionUrlSupplier.get());
+       log.info(getDbConnectionUrlSupplier.get().toString());
     }
 
     static String getDBConnectionUrl() {
         return "jdbc://localhost:5432/users";
     }
 
-    static Supplier<String> getDbConnectionUrlSupplier = () -> "jdbc://localhost:5432/users";
+    static Supplier<List<String>> getDbConnectionUrlSupplier = () -> 
+            List.of("jdbc://localhost:5432/users", "jdbc://localhost:5432/actions", "jdbc://localhost:5432/customers");
 
 }
