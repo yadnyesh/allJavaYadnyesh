681b3ae7a98e50ac05e4b9b42c5b5d2dd4d6cfa9
yadnyesh <yadnyesh.juvekar@gmail.com>
2021-07-13 09:54:27 +0530

    BiConsumer Example

--- src/main/java/io/yadnyesh/yt/amigoscode/javafunctionalprogramming/ConsumerYad.java
+++ src/main/java/io/yadnyesh/yt/amigoscode/javafunctionalprogramming/ConsumerYad.java
@@ -2,22 +2,30 @@ package io.yadnyesh.yt.amigoscode.javafunctionalprogramming;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 @Slf4j
 public class ConsumerYad {
+    static String middleString = "! thanks for registering with your phone: ";
+
     public static void main(String[] args) {
         greetCustomerImperative(new Customer("Yadnyesh", "9980057410"));
         greetCustomerFunctional.accept(new Customer("Yadnyesh", "9980057410"));
+        greetCustomerFunctionalV2.accept(new Customer("Yadnyesh", "9980057410"), false);
     }
 
     static void greetCustomerImperative (Customer customer) {
-       log.info("Hello " + customer.customerName
-               + "! thanks for registering with your phone: " + customer.customerPhoneNumber);
+        log.info("Hello " + customer.customerName
+               + middleString + customer.customerPhoneNumber);
     }
 
     static Consumer<Customer> greetCustomerFunctional = customer -> log.info("Consumer -> Hello " + customer.customerName
-            + "! thanks for registering with your phone: " + customer.customerPhoneNumber);
+            + middleString + customer.customerPhoneNumber);
+
+    static BiConsumer<Customer, Boolean> greetCustomerFunctionalV2 = (customer, showPhone) -> log.info("Consumer -> Hello " + customer.customerName
+            + middleString
+            + (Boolean.TRUE.equals(showPhone) ? customer.customerPhoneNumber : "Private Number"));
 
     static class Customer {
 
