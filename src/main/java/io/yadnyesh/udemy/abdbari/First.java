f15fed6e6e0690d465b55677905003d813b4a1ff
Yadnyesh <yadnyesh.juvekar@gmail.com>
2021-08-20 09:56:03 +0530

    Simple read input

--- src/main/java/io/yadnyesh/udemy/abdbari/First.java
+++ src/main/java/io/yadnyesh/udemy/abdbari/First.java
@@ -1,7 +1,12 @@
 package io.yadnyesh.udemy.abdbari;
 
+import java.util.Scanner;
+
 public class First {
     public static void main(String[] args) {
         System.out.println("Test");
+        Scanner scanner = new Scanner(System.in);
+        int firstnumber = scanner.nextInt();
+        System.out.println("Printing: " + firstnumber);
     }
 }
