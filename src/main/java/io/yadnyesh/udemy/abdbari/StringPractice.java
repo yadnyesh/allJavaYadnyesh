1a15843b1df1ed8968b4cb9d136b91c2389a0b99
yadnyesh <yadnyesh.juvekar@gmail.com>
2021-08-25 08:55:02 +0530

    String Regex

---
+++ src/main/java/io/yadnyesh/udemy/abdbari/StringPractice.java
@@ -0,0 +1,9 @@
+package io.yadnyesh.udemy.abdbari;
+
+public class StringPractice {
+    public static void main(String[] args) {
+        String str1 = "s8";
+        System.out.println(str1.matches("[a-z][0-9]"));
+    }
+
+}
