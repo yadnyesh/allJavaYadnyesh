cd515cb75ccec9ce32b8d737b55ee650c2d4c5c5
yadnyesh <yadnyesh.juvekar@gmail.com>
2021-08-26 09:03:41 +0530

    Switch Statement

--- src/main/java/io/yadnyesh/udemy/abdbari/StringPractice.java
+++ src/main/java/io/yadnyesh/udemy/abdbari/StringPractice.java
@@ -2,8 +2,22 @@ package io.yadnyesh.udemy.abdbari;
 
 public class StringPractice {
     public static void main(String[] args) {
-        String str1 = "s8";
-        System.out.println(str1.matches("[a-z][0-9]"));
+        String str1 = "yadnyesh";
+        System.out.println(str1.matches("[a-z]{5,10}"));
+
+        int i = 4;
+        switch (i)
+        {
+            case(1):
+                System.out.println("One");
+                break;
+            case(2):
+                System.out.println("Two");
+                break;
+            default:
+                System.out.println("Neither one nor two");
+        }
+
     }
 
 }
