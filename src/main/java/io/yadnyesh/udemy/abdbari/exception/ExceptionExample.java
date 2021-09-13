cbefc49b9c876efd59b9dcfecbf6f988a2b5bd68
yadnyesh <yadnyesh.juvekar@gmail.com>
2021-09-13 10:04:17 +0530

    Multiple catch blocks

--- src/main/java/io/yadnyesh/udemy/abdbari/exception/ExceptionExample.java
+++ src/main/java/io/yadnyesh/udemy/abdbari/exception/ExceptionExample.java
@@ -6,14 +6,17 @@ import lombok.extern.slf4j.Slf4j;
 public class ExceptionExample {
 
     public static void main(String[] args) {
-        int a = 10;
-        int b = 0;
-        int c = 0;
+        float c = 0;
+
+        int my_int_array[] = {10, 20, 40, 50, 60, 0};
 
         try{
-            c = a/b;
+            c = my_int_array[1]/my_int_array[6];
+            log.info("Result of Division is: " + c);
         } catch (ArithmeticException ae) {
             log.info("Exception: " + ae.getMessage());
+        } catch (ArrayIndexOutOfBoundsException aoob) {
+            log.info("Exception: " + aoob.getMessage());
         }
         log.info("Program continues post exception");
     }
