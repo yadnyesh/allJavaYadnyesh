77778be9509627d0d745c8e7f0db80baad86664a
yadnyesh <yadnyesh.juvekar@gmail.com>
2021-10-04 01:18:15 +0530

    Thread Info

--- src/main/java/io/yadnyesh/udemy/abdbari/multithreading/ThreadTest.java
+++ src/main/java/io/yadnyesh/udemy/abdbari/multithreading/ThreadTest.java
@@ -1,8 +1,20 @@
 package io.yadnyesh.udemy.abdbari.multithreading;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class ThreadTest {
     public static void main(String[] args) throws Exception {
         MyThread2 t = new MyThread2("My Thread");
+        //t.start();
+        log.info(String.valueOf(t.getId()));
+        log.info(t.getName());
+        log.info(String.valueOf(t.getState()));
+        log.info(String.valueOf(t.getPriority()));
+        log.info(String.valueOf(t.getThreadGroup()));
+
+
+
     }
 }
 
