f9bc183fe28607789fd56beb7cca4407779f6202
yadnyesh <yadnyesh.juvekar@gmail.com>
2021-10-05 09:17:43 +0530

    MultiThreading

--- src/main/java/io/yadnyesh/udemy/abdbari/multithreading/ThreadTest.java
+++ src/main/java/io/yadnyesh/udemy/abdbari/multithreading/ThreadTest.java
@@ -6,20 +6,22 @@ import lombok.extern.slf4j.Slf4j;
 public class ThreadTest {
     public static void main(String[] args) throws Exception {
         MyThread2 t = new MyThread2("My Thread");
-        //t.start();
-        log.info(String.valueOf(t.getId()));
-        log.info(t.getName());
-        log.info(String.valueOf(t.getState()));
-        log.info(String.valueOf(t.getPriority()));
-        log.info(String.valueOf(t.getThreadGroup()));
-
-
-
+        int count = 1;
+        while (true) {
+            log.info(count++ + " ThreadTest");
+        }
     }
 }
 
+@Slf4j
 class MyThread2 extends Thread {
     public MyThread2(String name) {
         super(name);
+    }
+    public void run() {
+        int count = 1;
+        while (true) {
+            log.info(count++ + " My Thread2");
+        }
     }
 }
