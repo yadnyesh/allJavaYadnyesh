a77e7a9404dc7e42213b0eff44503c809a6423e3
Yadnyesh <yadnyesh.juvekar@walmart.com>
2022-01-04 13:47:56 +0530

    Generic Example

--- src/main/java/io/yadnyesh/yt/misc/GenericsExample.java
+++ src/main/java/io/yadnyesh/yt/misc/GenericsExample.java
@@ -1,8 +1,20 @@
 package io.yadnyesh.yt.misc;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+
+@Slf4j
 public class GenericsExample {
     public static void main(String[] args) {
         GenericPrinter<Integer> genericPrinter = new GenericPrinter<>(10);
         genericPrinter.print();
+
+        ArrayList<String> cats = new ArrayList<>();
+        cats.add("Cat 1");
+        cats.add("Cat 2");
+        cats.add("Cat 3");
+        cats.add("Cat 4");
+        log.info(String.valueOf(cats.size()));
     }
 }
