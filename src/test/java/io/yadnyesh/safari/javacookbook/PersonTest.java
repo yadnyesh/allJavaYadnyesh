418c22c44fd2b108a21a64ad6f86070cd233488c
Yadnyesh <yadnyesh.juvekar@walmart.com>
2022-01-04 13:37:08 +0530

    Initial commit

--- src/test/java/io/yadnyesh/safari/javacookbook/PersonTest.java
+++ src/test/java/io/yadnyesh/safari/javacookbook/PersonTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
-    @Test
+
     public void testNameConcatenate() {
         Person yadnyesh = new Person("Yadnyesh", "Juvekar");
         String fullName = yadnyesh.getFullname();
