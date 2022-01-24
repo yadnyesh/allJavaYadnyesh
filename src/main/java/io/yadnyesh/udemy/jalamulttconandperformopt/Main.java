c4daa3b5fc7803f1927e3ea0e4552b05051c1274
yadnyesh <yadnyesh.juvekar@gmail.com>
2022-01-24 18:24:57 +0530

    Exception handling in Threads

--- src/main/java/io/yadnyesh/udemy/jalamulttconandperformopt/Main.java
+++ src/main/java/io/yadnyesh/udemy/jalamulttconandperformopt/Main.java
@@ -5,12 +5,20 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Thread thread = new Thread(()
-                -> log.info("Currently in Thread: " + Thread.currentThread().getName()));
+        Thread thread = new Thread(() -> {
+            log.info("Currently in Thread: " + Thread.currentThread().getName());
+            throw new RuntimeException("intentional exception");
+        });
         thread.setName("New Worker Thread");
         log.info("Before starting thread: " + Thread.currentThread().getName());
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler(){
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                log.info("A critical error has happened in thread: " + t.getName());
+            }
+        });
         thread.start();
-        thread.setPriority(10);
+        thread.setPriority(Thread.MAX_PRIORITY);
         log.info("After starting thread: " + Thread.currentThread().getName());
         log.info("Thread Priority: " + Thread.currentThread().getPriority());
         Thread.sleep(2000);
