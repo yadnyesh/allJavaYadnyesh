bddff4d231445ce9cdd7c657543c9e9e20df692d
yadnyesh <yadnyesh.juvekar@gmail.com>
2022-01-24 18:14:50 +0530

    Name the new Thread

--- src/main/java/io/yadnyesh/udemy/jalamulttconandperformopt/Main.java
+++ src/main/java/io/yadnyesh/udemy/jalamulttconandperformopt/Main.java
@@ -5,12 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                log.info("Currently in Thread: " + Thread.currentThread().getName());
-            }
-        });
+        Thread thread = new Thread(()
+                -> log.info("Currently in Thread: " + Thread.currentThread().getName()));
+        thread.setName("New Worker Thread");
         log.info("Before starting thread: " + Thread.currentThread().getName());
         thread.start();
         log.info("After starting thread: " + Thread.currentThread().getName());
