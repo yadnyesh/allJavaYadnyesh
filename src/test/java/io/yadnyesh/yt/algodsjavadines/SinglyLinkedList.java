228496733c7aad03680a570ed67db3836e1929c2
Yadnyesh <yadnyesh.juvekar@walmart.com>
2022-01-26 14:15:19 +0530

    Starting with SinglyLinkedList

--- src/test/java/io/yadnyesh/yt/algodsjavadines/SinglyLinkedList.java
+++ src/test/java/io/yadnyesh/yt/algodsjavadines/SinglyLinkedList.java
@@ -1,5 +1,9 @@
 package io.yadnyesh.yt.algodsjavadines;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.List;
+
 public class SinglyLinkedList {
 
     private ListNode head;
@@ -13,5 +17,18 @@ public class SinglyLinkedList {
             this.next = null;
         }
     }
+
+    public static void main(String[] args) {
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        singlyLinkedList.head = new ListNode(10);
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(2);
+        ListNode fourth = new ListNode(3);
+
+        singlyLinkedList.head.next = second;
+        second.next = third;
+        third.next = fourth;
+    }
+
 
 }
