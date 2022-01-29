65778b0eb3f02734500ed226aa7833f388177ce9
yadnyesh <yadnyesh.juvekar@gmail.com>
2022-01-29 16:18:34 +0530

    Factorial

--- src/main/java/io/yadnyesh/udemy/javadsnalgo/Recursion1.java
+++ src/main/java/io/yadnyesh/udemy/javadsnalgo/Recursion1.java
@@ -10,8 +10,17 @@ public class Recursion1 {
         System.out.println("Hello World " + n);
     }
 
+    public int factorial(int n) {
+        if ( n == 0) {
+            return 1;
+        } else {
+            return ( n * factorial(n - 1));
+        }
+    }
+
     public static void main(String[] args) {
         Recursion1 recursion1 = new Recursion1();
         recursion1.foo(3);
+        System.out.println("Factorial is: " + recursion1.factorial(5));
     }
 }
