e2348471220dfac741ff9fb4be2afdd8e0959c42
Yadnyesh <yadnyesh.juvekar@walmart.com>
2022-06-18 11:28:39 +0530

    Raise a number to power of 2

---
+++ src/main/java/io/yadnyesh/udemy/dsalgomasterclass/PowerOfTwo.java
@@ -0,0 +1,22 @@
+package io.yadnyesh.udemy.dsalgomasterclass;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        int raiseTwoTo = 4;
+        int result = powerOfTwo(raiseTwoTo);
+        log.info(String.valueOf(result));
+    }
+
+    private static int powerOfTwo(int raiseTwoTo) {
+        if(raiseTwoTo == 0) {
+            return 1;
+        }
+        else
+            return 2 * powerOfTwo(raiseTwoTo - 1);
+    }
+
+
+}
