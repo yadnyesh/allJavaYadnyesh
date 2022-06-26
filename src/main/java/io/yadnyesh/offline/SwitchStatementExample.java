ba6dd5ad0dea3931ce341ba7c204f0f9df9a6a59
Yadnyesh <yadnyesh.juvekar@walmart.com>
2022-06-26 19:45:23 +0530

    Switch statement example

--- src/main/java/io/yadnyesh/offline/SwitchStatementExample.java
+++ src/main/java/io/yadnyesh/offline/SwitchStatementExample.java
@@ -2,13 +2,17 @@ package io.yadnyesh.offline;
 
 public class SwitchStatementExample {
     public static void main(String[] args) {
-        Integer x = new Integer(10);
+        Integer x = new Integer(1);
         switch (x) {
-            case  (10):
-                System.out.println("Ten");
-            break;
-            case  (20):
-                System.out.println("Twenty");
+            case  (0):
+                System.out.println("0");
+            case  (1):
+                System.out.println("1");
+                break;
+            case (2):
+                System.out.println(2);
+            default:
+                System.out.println("default");
         }
     }
 }
