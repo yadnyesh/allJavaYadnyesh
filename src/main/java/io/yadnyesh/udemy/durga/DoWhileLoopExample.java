9469097de8421c1e1a6bbd5e822f3f131e37b574
Yadnyesh <yadnyesh.juvekar@walmart.com>
2022-07-04 10:27:26 +0530

    Do While statement example

---
+++ src/main/java/io/yadnyesh/udemy/durga/DoWhileLoopExample.java
@@ -0,0 +1,11 @@
+package io.yadnyesh.udemy.durga;
+
+public class DoWhileLoopExample {
+    public static void main(String[] args) {
+        int i = 0;
+        do{
+            System.out.println("Do While: " + i);
+            i++;
+        } while ( i < 3);
+    }
+}
