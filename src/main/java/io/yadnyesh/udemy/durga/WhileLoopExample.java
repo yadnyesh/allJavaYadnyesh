0a65ee91a6d2ede3c893fe666cb74b57bf90e32a
Yadnyesh <yadnyesh.juvekar@walmart.com>
2022-07-04 10:24:17 +0530

    While statement example

---
+++ src/main/java/io/yadnyesh/udemy/durga/WhileLoopExample.java
@@ -0,0 +1,11 @@
+package io.yadnyesh.udemy.durga;
+
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 3) {
+            System.out.println("Hello: " + i);
+            i++;
+        }
+    }
+}
