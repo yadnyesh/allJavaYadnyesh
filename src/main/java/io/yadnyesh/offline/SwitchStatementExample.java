bc77fd7212d8d6cb7820810b8c3bdd5794febb10
Yadnyesh <yadnyesh.juvekar@walmart.com>
2022-07-04 10:12:38 +0530

    Switch statement example - expression allowed

--- src/main/java/io/yadnyesh/offline/SwitchStatementExample.java
+++ src/main/java/io/yadnyesh/offline/SwitchStatementExample.java
@@ -2,15 +2,18 @@ package io.yadnyesh.offline;
 
 public class SwitchStatementExample {
     public static void main(String[] args) {
-        Integer x = new Integer(1);
-        switch (x) {
-            case  (0):
-                System.out.println("0");
+        int x = 10;
+        int y = 20;
+
+        switch (x+1) {
+            case  (10):
+                System.out.println("10");
             case  (1):
                 System.out.println("1");
                 break;
-            case (2):
-                System.out.println(2);
+            case (11):
+                System.out.println("11");
+                break;
             default:
                 System.out.println("default");
         }
