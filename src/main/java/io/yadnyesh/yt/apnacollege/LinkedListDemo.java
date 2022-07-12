41e472a40aa671a3e62aeba5956e5b515cdd7885
Yadnyesh <yadnyesh.juvekar@walmart.com>
2022-07-12 10:36:05 +0530

    Linkedlist Done.

---
+++ src/main/java/io/yadnyesh/yt/apnacollege/LinkedListDemo.java
@@ -0,0 +1,59 @@
+package io.yadnyesh.yt.apnacollege;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LinkedListDemo {
+    Node head = null;
+
+    class Node {
+        String data;
+        Node next;
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void addLast(String data) {
+        Node lastNode = new Node(data);
+        if (head == null) {
+            head = lastNode;
+            return;
+        }
+        Node currNode = head;
+        while(currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = lastNode;
+    }
+
+    public void printLinkedList() {
+        Node currNode = head;
+        while(currNode != null) {
+            System.out.println(currNode.data);
+            currNode = currNode.next;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        LinkedListDemo linkedList = new LinkedListDemo();
+        linkedList.addFirst("Yadnyesh");
+        linkedList.addFirst("Bharat");
+        linkedList.addFirst("Juvekar");
+        linkedList.addLast("Mr.");
+        linkedList.printLinkedList();
+    }
+}
