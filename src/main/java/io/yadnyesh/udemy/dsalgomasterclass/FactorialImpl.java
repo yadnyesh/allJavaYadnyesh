36b733e8d080c0179506ab9eb33671ae318d7209
Yadnyesh <yadnyesh.juvekar@gmail.com>
2022-07-13 09:44:20 +0530

    Factorial Recursive Implementation --> Udemy

---
+++ src/main/java/io/yadnyesh/udemy/dsalgomasterclass/FactorialImpl.java
@@ -0,0 +1,16 @@
+package io.yadnyesh.udemy.dsalgomasterclass;
+
+public class FactorialImpl {
+
+    public int factorial(int n) {
+        if( n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n-1);
+    }
+
+    public static void main(String[] args) {
+        FactorialImpl factorial = new FactorialImpl();
+        System.out.println(factorial.factorial(5));
+    }
+}
