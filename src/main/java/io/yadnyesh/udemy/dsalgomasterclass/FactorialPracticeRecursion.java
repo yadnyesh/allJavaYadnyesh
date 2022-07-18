5cd55998323a94099454cffd5f0e49732dd44774
Yadnyesh <yadnyesh.juvekar@gmail.com>
2022-07-18 21:08:49 +0530

    Factorial In Loop --> Udemy

--- src/main/java/io/yadnyesh/udemy/dsalgomasterclass/FactorialPracticeRecursion.java
+++ src/main/java/io/yadnyesh/udemy/dsalgomasterclass/FactorialPracticeRecursion.java
@@ -2,16 +2,30 @@ package io.yadnyesh.udemy.dsalgomasterclass;
 
 public class FactorialPracticeRecursion {
 
-    public static int factorial(int n) {
+    public static int factorialRecursion(int n) {
         if ( n == 0  || n ==1) {
             return 1;
         }
 
-        return n * factorial(n-1);
+        return n * factorialRecursion(n-1);
+    }
+
+
+    public static int factorialLoop(int n) {
+        int sum = 1;
+
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            sum = sum * i;
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
-        int nFactorial = factorial(5);
-        System.out.println(nFactorial);
+        int nFactorial = factorialRecursion(5);
+        int nFactorialInLoop = factorialLoop(5);
+        System.out.println(nFactorialInLoop);
     }
 }
